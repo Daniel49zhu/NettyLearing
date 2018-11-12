25de1197d18bcf0b249efeefab241f5c3358bdad
Daniel49zhu <zhujiacheng94@163.com>
2018-11-12 22:35:07 +0800

    TestHttpServletHandler

--- src/main/java/com/zjc/netty/firstexample/TestHttpServerHandler.java
+++ src/main/java/com/zjc/netty/firstexample/TestHttpServerHandler.java
@@ -10,13 +10,17 @@ import io.netty.util.CharsetUtil;
 public class TestHttpServerHandler extends SimpleChannelInboundHandler<HttpObject> {
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
-        ByteBuf content = Unpooled.copiedBuffer("Hello World", CharsetUtil.UTF_8);
-        FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1,
-                                                                HttpResponseStatus.OK,
-                                                                content);
-        response.headers().set(HttpHeaderNames.CONTENT_TYPE,"text/plain");
-        response.headers().set(HttpHeaderNames.CONTENT_LENGTH,content.readableBytes());
+        if(msg instanceof HttpRequest) {
+            HttpRequest httpRequest = (HttpRequest)msg;
+            System.out.println("请求方法名："+httpRequest.method().name());
+            ByteBuf content = Unpooled.copiedBuffer("Hello World", CharsetUtil.UTF_8);
+            FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1,
+                    HttpResponseStatus.OK,
+                    content);
+            response.headers().set(HttpHeaderNames.CONTENT_TYPE,"text/plain");
+            response.headers().set(HttpHeaderNames.CONTENT_LENGTH,content.readableBytes());
 
-        ctx.writeAndFlush(response);
+            ctx.writeAndFlush(response);
+        }
     }
 }
