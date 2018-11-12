71b598ed17c26111bcbcdb914ac4c633335508b6
Daniel49zhu <zhujiacheng94@163.com>
2018-11-12 21:53:04 +0800

    第一个例子

---
+++ src/main/java/com/zjc/netty/firstexample/TestServer.java
@@ -0,0 +1,30 @@
+package com.zjc.netty.firstexample;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class TestServer {
+    public static void main(String[] args) throws Exception{
+        //两个事件循环组，boss接收连接，然后发送给worker
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            //用于轻松启动服务端的一个类
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup,workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new TestServerInitializer());
+
+            ChannelFuture channelFuture = serverBootstrap.bind(8899).sync();
+            channelFuture.channel().closeFuture().sync();
+        }
+        finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
+    }
+}
