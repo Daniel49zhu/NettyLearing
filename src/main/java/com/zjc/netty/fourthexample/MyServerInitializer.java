68e70f0bbabfc133626a2ceb93b14861c74b6759
Daniel49zhu <zhujiacheng94@163.com>
2018-11-18 20:08:44 +0800

    第四个例子，心跳检测

---
+++ src/main/java/com/zjc/netty/fourthexample/MyServerInitializer.java
@@ -0,0 +1,16 @@
+package com.zjc.netty.fourthexample;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.timeout.IdleStateHandler;
+
+public class MyServerInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+
+        pipeline.addLast(new IdleStateHandler(5,7,10));//空闲状态检测的处理器
+        pipeline.addLast(null);
+    }
+}
