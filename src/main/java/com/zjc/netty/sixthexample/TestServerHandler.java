89f0b2bf543eb3eef1201af18783c7f61de91d1c
Daniel49zhu <zhujiacheng94@163.com>
2018-11-20 21:34:03 +0800

    protobuf_server

--- src/main/java/com/zjc/netty/sixthexample/TestServerHandler.java
+++ src/main/java/com/zjc/netty/sixthexample/TestServerHandler.java
@@ -6,6 +6,9 @@ import io.netty.channel.SimpleChannelInboundHandler;
 public class TestServerHandler extends SimpleChannelInboundHandler<DataInfo.Student> {
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, DataInfo.Student msg) throws Exception {
-
+        //因为已经添加了Decoder和Encoder 所以已经进行了反序列化
+        System.out.println(msg.getAddress());
+        System.out.println(msg.getName());
+        System.out.println(msg.getAge());
     }
 }
