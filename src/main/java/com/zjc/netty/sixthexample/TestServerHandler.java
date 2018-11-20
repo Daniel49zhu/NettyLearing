7cf55af0b1e52177d05bc2e16889a573ad4c3c73
Daniel49zhu <zhujiacheng94@163.com>
2018-11-20 21:14:55 +0800

    protobuf_server

--- src/main/java/com/zjc/netty/sixthexample/TestServerHandler.java
+++ src/main/java/com/zjc/netty/sixthexample/TestServerHandler.java
@@ -3,9 +3,9 @@ package com.zjc.netty.sixthexample;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
-public class TestServerHandler extends SimpleChannelInboundHandler<String> {
+public class TestServerHandler extends SimpleChannelInboundHandler<DataInfo.Student> {
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+    protected void channelRead0(ChannelHandlerContext ctx, DataInfo.Student msg) throws Exception {
 
     }
 }
