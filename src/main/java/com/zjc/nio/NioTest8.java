768b042821f39f288e4521a21b7fa1bb9327f568
Daniel49zhu <zhujiacheng94@163.com>
2018-11-28 21:27:33 +0800

    nio

---
+++ src/main/java/com/zjc/nio/NioTest8.java
@@ -0,0 +1,31 @@
+package com.zjc.nio;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class NioTest8 {
+    public static void main(String[] args) throws Exception{
+        FileInputStream inputStream = new FileInputStream("input2.txt");
+        FileOutputStream outputStream = new FileOutputStream("output2.txt");
+
+        FileChannel inputChannel = inputStream.getChannel();
+        FileChannel outputChannel = outputStream.getChannel();
+
+        ByteBuffer buffer = ByteBuffer.allocateDirect(1 << 10);//DirectByteBuffer
+        while (true) {
+            buffer.clear();
+            int read = inputChannel.read(buffer);
+            if (-1 == read) {
+                break;
+            }
+            buffer.flip();
+
+            outputChannel.write(buffer);
+        }
+
+        inputStream.close();
+        outputStream.close();
+    }
+}
